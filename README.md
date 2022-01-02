d7e3a9894e6a85b4b63b18b2d884f33874081a35
adityagarde <aditya.garde96@gmail.com>
2022-01-02 17:45:38 +0530

    Go

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## go-ticket-booking-app
 
 Simple CLI application which books ticket for a Go conference made to learn the fundamentals of Go programming language.
+
+### Goroutine vs OS Threads
+- Go doesnt directly use OS level threads, it uses `Green Threads` which are an `abstraction` over an actual thread.
+- `Goroutines` are managed by the go runtime, we are only interacting with these high level go routines. While OS thrads are managed by kernel.
+- These Goroutines are `cheaper` and `lightweight` while OS threads are costly and take some time to start up. Thus we can run thousands of go routines without affecting the performance much.
+
+- Also, the have built-in functionality called `Channels` for internal communication i.e. receiving and sending data. Comparitively it is difficult to communicate in OS Threads.
--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"go-booking-app/helper"
-	"strings"
+	"sync"
+	"time"
 )
 
 const conferenceTickets = 50
 
 var conferenceName = "Go Conference"
 var remainingTickets uint = conferenceTickets
-var bookings = []string{}
+var bookings = make([]UserData, 1)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+// Waits for the launched goroutine to finish.
+var waitGroup = sync.WaitGroup{}
 
 func main() {
 
@@ -24,6 +35,9 @@ func main() {
 		if isValidName && isValidEmail && isValidUserTicketNumber {
 
 			bookTickets(userTickets, firstName, lastName, email)
+
+			waitGroup.Add(1) // Add - increases the counter by the number of goroutines to wait for.
+			go sendTickets(userTickets, firstName, lastName, email)
 
 			firstNames := getFirstNames()
 			fmt.Printf("These are all the bookings - %v\n", firstNames)
@@ -44,6 +58,7 @@ func main() {
 			}
 		}
 	}
+	waitGroup.Wait()
 }
 
 func greetUsers() {
@@ -56,8 +71,7 @@ func getFirstNames() []string {
 	firstNames := []string{}
 
 	for _, booking := range bookings { // _ is Blank Identifier - Ignoring unused variable
-		names := strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -82,8 +96,26 @@ func getUserInput() (string, string, string, uint) {
 
 func bookTickets(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings - %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+}
+
+func sendTickets(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v\n", userTickets, firstName, lastName)
+	fmt.Println("####################")
+	fmt.Printf("Sending ticket %v to email %v\n", ticket, email)
+	fmt.Println("####################")
+	waitGroup.Done()
 }
